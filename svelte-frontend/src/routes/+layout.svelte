7bafcc5ca924d8beb94a846813ed620a27777bf9
raoul grouls <Raoul.Grouls@han.nl>
2025-06-17 12:58:31 +0200

    clean up frontend for studio

--- svelte-frontend/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  // This line imports all our global styles and Tailwind CSS definitions,
-  // making them available to the entire application.
-  import '../app.css';
-</script>
-
-<slot />
